a15c927ea086949a4e8c6c6179d54a24971bb99d
Callum J Harding <callumhdg@gmail.com>
2021-05-27 19:58:20 +0100

    Added primary function.

--- Raindrops/Raindrops.cs
+++ Raindrops/Raindrops.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Raindrops
 {
-    class Raindrops
+    public class Raindrops
     {
+
+        public string PlingPlangPlong(int input)
+        {
+            StringBuilder sb = new StringBuilder();
+            bool isAFactor = false;
+
+            //is input divisable by three?
+            if (input % 3 == 0)
+            {
+                sb.Append("Pling");
+                isAFactor = true;
+            }
+
+            //is input divisable by five?
+            if (input % 5 == 0)
+            {
+                sb.Append("Plang");
+                isAFactor = true;
+            }
+
+            //is input divisable by seven?
+            if (input % 7 == 0)
+            {
+                sb.Append("Plong");
+                isAFactor = true;
+            }
+
+            //if input is a factor return string of "Pling" and or "Plang" and or "Plong", else return input
+            return (isAFactor == true) ? sb.ToString() : input.ToString();
+
+        }
+
+
     }
 }
